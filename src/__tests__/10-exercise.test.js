11514007b79da514660c06f23db608c0e248a751
andarbech <andarbech1.com>
2021-12-06 18:47:34 +0100

    finish

--- src/__tests__/10-exercise.test.js
+++ src/__tests__/10-exercise.test.js
@@ -4,25 +4,27 @@ import { addButton } from "../utils/dom";
  * Write the assertions using the most appropriate matcher
  */
 describe("10-exercises", () => {
-  beforeAll(() => {
-    // 1. set the default inner html of the document body
-    //    to have a div with a class of .wrapper
-  });
+    beforeAll(() => {
+        // 1. set the default inner html of the document body
+        //    to have a div with a class of .wrapper
+        document.body.innerHTML = `<div class="wrapper></div>"`;
+    });
 
-  test("adds a button to the dom", () => {
-    // You should make 1 assertion in this test, see bellow
-    expect.assertions(1);
+    test("adds a button to the dom", () => {
+        // You should make 1 assertion in this test, see bellow
+        expect.assertions(1);
 
-    const btnText = "Save";
+        const btnText = "Save";
 
-    // 2. execute the addButton function and pass it as an argument
-    //    the `btnText` variable
-
-    // 3. use document.querySelector to get the new button
-    //    the `addButton` creates a button with a class of .btn
-    //    that you can use in document.querySelector
-
-    // 4. make an assertion that the buttons textContent is
-    //    the same as the `btnText` variable
-  });
+        // 2. execute the addButton function and pass it as an argument
+        //    the `btnText` variable
+        addButton(btnText);
+        // 3. use document.querySelector to get the new button
+        //    the `addButton` creates a button with a class of .btn
+        //    that you can use in document.querySelector
+        newButton = document.querySelector(".btn")
+            // 4. make an assertion that the buttons textContent is
+            //    the same as the `btnText` variable
+        expect(newButton.textContent).toBe(btnText)
+    });
 });
